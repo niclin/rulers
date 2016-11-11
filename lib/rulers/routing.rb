935d788fdc7ab77af64bf716d20b0e1b440715a8
niclin <bboyceo@hotmail.com>
2016-11-11 16:00:15 +0800

    Fix bugs

--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -1,7 +1,7 @@
 module Rulers
-  class Appliaction
+  class Application
     def get_controller_and_action(env)
-      _,cont, action, after =
+      _, cont, action, after =
         env["PATH_INFO"].split('/', 4)
       cont = cont.capitalize # "People"
       cont += "Controller" # PeopleController
